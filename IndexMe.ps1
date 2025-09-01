e6544243fc975674b15db6913bc03449e7fc344d
HuskyMoose <93372404+HuskyMoose@users.noreply.github.com>
2025-09-01 17:09:10 -0400

    Update IndexMe.ps1

--- IndexMe.ps1
+++ IndexMe.ps1
@@ -1,1 +1,4 @@
-gci -Recurse | Select-Object -Property Name,Length,Directory,Extension,@{name="MD5_Hash";expression={(Get-FileHash -Algorithm MD5 $_.FullName).hash}}, @{name="SHA1_Hash";expression={(Get-FileHash -Algorithm SHA1 $_.FullName).hash}} | Export-CSV Index.csv 
+$md5hash = @{name="MD5_Hash";expression={(Get-FileHash -Algorithm MD5 $_.FullName).hash}}
+$sha1hash = @{name="SHA1_Hash";expression={(Get-FileHash -Algorithm SHA1 $_.FullName).hash}}
+
+get-ChildItem -Recurse -File  * | Select-Object -Property Name,Length,Directory,Extension, $md5hash, $sha1hash | Export-CSV Index.csv -NoTypeInformation
